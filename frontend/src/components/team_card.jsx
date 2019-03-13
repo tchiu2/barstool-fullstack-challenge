fea05bf8b8dcbedd3aa8a87dd0cbd2594e8d0481
Terence Chiu <tchiu2@gmail.com>
2019-03-13 16:20:33 -0400

    update styling for small screen sizes

--- frontend/src/components/team_card.jsx
+++ frontend/src/components/team_card.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const TeamCard = ({ team, home }) => (
   <div className={`boxscore-details-team${home === "true" ? " home" : " away"} flex-container vertical`}>
     <p>
-      <strong>{team.last_name}</strong>
+      <strong className="team-name hide-full">{team.last_name}</strong>
+      <strong className="abbreviation hide-abbr">{team.abbreviation}</strong>
     </p>
     <span>36-18</span>
   </div>
